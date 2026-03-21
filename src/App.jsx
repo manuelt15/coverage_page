2e7102e40f63ad4267e0831de740b9b91a783ed5
Manuel Torres <manueltorres1512@gmail.com>
2026-03-21 18:42:51 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className="app">
       <header className="app-header">
-        <span className="logo">rollee</span>
+        <span className="logo">Company</span>
         <span className="coverage-label">
           Coverage <small>{data.length} platforms in total</small>
         </span>
